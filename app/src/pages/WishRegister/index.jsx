53a7f2b398ea982510c58bec3f163a1e17bb05af
Janis <feldmanis45@gmail.com>
2020-03-27 19:23:32 +0200

    added register wish

--- app/src/pages/WishRegister/index.jsx
+++ app/src/pages/WishRegister/index.jsx
@@ -1,60 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import { message } from 'antd';
+import React, {useState} from 'react';
+import { Form, Row, Col, message } from 'antd';
+import { withRouter } from 'react-router-dom';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import Button from 'components/Button';
+import { NavLink } from 'react-router-dom';
+import Input from 'components/Input';
 import APIClient from 'utils/apiClient';
-
-import { Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-
+import jwt from 'utils/jwt';
 import './styles.scss';
 
 
 const WishRegister = () => {
-  
-  const [walletActions, setWalletActions] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+
+
   const [loading, setLoading] = useState(false);
+
   const onFinish = async (values) => {
     try {
       setLoading(true);
       let response = await APIClient.request(
         '/api/test/register-wish',
-        {
-          username: values.username, email: values.email, password: values.password, roles: ["user"]
-        },
+        {wish: values.username}, // it gives wish
         'POST'
       );
-      // jwt.saveToken(response.token, response.expiresIn);
+      message.info("wish registered");
       setLoading(false);
-      message.info("You are sucesfully registered");
-      // history.push("/login");
-      //   NavLink.to("/login");
+
     } catch (err) {
-      message.error("Username or password incorrect!");
+      message.error("Wish is not registered");
       console.log(err);
       setLoading(false);
     }
   };
 
 
+
   return (
-    <>
-
-<TextField
-          id="outlined-full-width"
-          label="Label"
-          style={{ margin: 8 }}
-          placeholder="Placeholder"
-          helperText="Full width!"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
+    <>    <Row align="middle" justify="center" className="h-100" >
+    <Col xs={22} sm={16} md={12} lg={8}>
+      <div className="login-card">
+        <img src="betit-logo-dark.svg" alt="betit logo" className="betit-logo" height={25} />
+        <h3>Please register a wish</h3>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
           }}
-          variant="outlined"
-          color='color="secondary'
-        />
-
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+         
+          <Form.Item>
+            <Button loading={loading} block type="primary" htmlType="submit" className="login-form-button">
+              Register a wish
+            </Button>
+      
+            
+          </Form.Item>
+        </Form>
+      </div>
+    </Col>
+  </Row>
     </>
 
   )
